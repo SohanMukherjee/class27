07f196969385cdbfed7a976d6b0e6d11583ba212
SohanMukherjee <73114040+SohanMukherjee@users.noreply.github.com>
2020-11-24 17:55:00 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,20 @@
+class Slingshot {
+    constructor(body1,body2){
+        var options = {
+            bodyA:body1,
+            bodyB:body2,
+            length:10,
+            stiffness:0.04
+        }
+        this.chain = Matter.Constraint.create(options)
+        World.add(world,this.chain)
+    }
+
+    display(){
+        var point1 = this.chain.bodyA.position;
+        var point2 = this.chain.bodyB.position;
+        strokeWeight(4)
+        line (point1.x,point1.y,point2.x,point2.y)
+    }
+       
+}
